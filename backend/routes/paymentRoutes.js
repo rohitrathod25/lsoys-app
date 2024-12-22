7e70f769cd343d13bac87ed59f2fc6c1202f582c
rohitrathod25 <rathorohit37@gmail.com>
2024-12-22 12:52:43 +0530

    Delete backend directory

--- backend/routes/paymentRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const paymentController = require('../controllers/paymentController');
-
-router.post('/order', paymentController.createOrder);
-
-module.exports = router;
--- backend/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: '30d', // Set your desired expiration time
-    });
-};
-
-module.exports = generateToken;
